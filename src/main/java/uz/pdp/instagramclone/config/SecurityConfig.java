815ba23ae26aa85a97dc05036cb949427b09b6e2
Jo'raqulov O'tkirjon <97774479+FranchiscoMike@users.noreply.github.com>
2022-04-12 11:39:55 +0500

    some changes in project user ,post, message is done.s

--- src/main/java/uz/pdp/instagramclone/config/SecurityConfig.java
+++ src/main/java/uz/pdp/instagramclone/config/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
-                .antMatchers("/api/**").permitAll()
+                .antMatchers("/**").permitAll()
                 .anyRequest()
                 .authenticated();
         http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
